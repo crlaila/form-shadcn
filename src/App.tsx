bcd262c4675ecda155902888651a3cd82d133b1b
Laila Silva <laila.cristine@outlook.com>
2024-09-02 23:49:37 -0300

    feat: validacao do form com zod

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,48 @@ import {
   SelectTrigger,
   SelectValue,
 } from './components/ui/select';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-// Array pra dias do mês de 1 a 31
+// Constantes criadas para seleção de data de nascimento
 const days = Array.from({ length: 31 }, (_, i) => i + 1);
 const months = Array.from({ length: 12 }, (_, i) => i + 1);
 const currentYear = new Date().getFullYear();
 const years = Array.from({ length: 50 }, (_, i) => currentYear - i);
 
+const schema = z.object({
+  name: z.string().nonempty({ message: 'Nome é obrigatório' }),
+  surname: z.string().nonempty({ message: 'Sobrenome é obrigatório' }),
+  email: z.string().email({ message: 'E-mail inválido' }),
+  company: z.string().optional(),
+  day: z.number().int().min(1).max(31),
+  month: z.number().int().min(1).max(12),
+  year: z
+    .number()
+    .int()
+    .min(currentYear - 50)
+    .max(currentYear),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export function App() {
+  // Função para registrar os campos do formulário
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  // Função para submeter os dados do formulário
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <div className="h-screen w-full flex items-center justify-center bg-zinc-900">
       <div className="w-full max-w-2xl bg-zinc-800 shadow rounded p-8">
@@ -24,21 +58,37 @@ export function App() {
           Cadastro
         </h1>
 
-        <form action="" className="mt-8 flex flex-col gap-4">
+        <form
+          action=""
+          className="mt-8 flex flex-col gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="grid grid-cols-2 gap-8">
             <div>
               <Label className="text-white">Nome</Label>
               <Input
                 type="text"
+                {...register('name')}
                 className="text-white border-green-100 mt-2 space-y-3"
               />
+              {errors.name && (
+                <span className="text-red-500 text-xs">
+                  {errors.name.message}
+                </span>
+              )}
             </div>
             <div>
               <Label className="text-white">Sobrenome</Label>
               <Input
                 type="text"
+                {...register('surname')}
                 className="text-white border-green-100 mt-2 space-y-3"
               />
+              {errors.surname && (
+                <span className="text-red-500 text-xs">
+                  {errors.surname.message}
+                </span>
+              )}
             </div>
           </div>
 
@@ -47,15 +97,27 @@ export function App() {
               <Label className="text-white">E-mail</Label>
               <Input
                 type="email"
+                {...register('email')}
                 className="text-white border-green-100 mt-2 space-y-3"
               />
+              {errors.email && (
+                <span className="text-red-500 text-xs">
+                  {errors.email.message}
+                </span>
+              )}
             </div>
             <div>
               <Label className="text-white">Empresa</Label>
               <Input
                 type="text"
+                {...register('company')}
                 className="text-white border-green-100 mt-2 space-y-3"
               />
+              {errors.company && (
+                <span className="text-red-500 text-xs">
+                  {errors.company.message}
+                </span>
+              )}
             </div>
           </div>
 
@@ -63,7 +125,9 @@ export function App() {
             <div>
               <Label className="text-white">Data de Nascimento</Label>
               <div className="mt-2 text-white">
-                <Select>
+                <Select
+                  onValueChange={(value) => setValue('day', Number(value))}
+                >
                   <SelectTrigger className="w-full">
                     <SelectValue placeholder="Dia" />
                   </SelectTrigger>
@@ -80,7 +144,9 @@ export function App() {
 
             <div>
               <div className="mt-2 text-white">
-                <Select>
+                <Select
+                  onValueChange={(value) => setValue('month', Number(value))}
+                >
                   <SelectTrigger className="w-full">
                     <SelectValue placeholder="Mês" />
                   </SelectTrigger>
@@ -97,7 +163,9 @@ export function App() {
 
             <div>
               <div className="mt-2 text-white">
-                <Select>
+                <Select
+                  onValueChange={(value) => setValue('year', Number(value))}
+                >
                   <SelectTrigger className="w-full">
                     <SelectValue placeholder="Ano" />
                   </SelectTrigger>
